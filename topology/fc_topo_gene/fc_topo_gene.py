0e4142f8d1c02a735d4a94a5758a4336f0a39a15
zx <zhangxiao123@sjtu.edu.cn>
2022-12-15 06:01:50 +0800

    Add the code to develop the subgraph for routing
    by zx

--- topology/fc_topo_gene/fc_topo_gene.py
+++ topology/fc_topo_gene/fc_topo_gene.py
@@ -13,6 +13,7 @@ def fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random):
     bipart_num = len(vir_layer_degree) - 1
     switch_list = [i for i in range(switches)]
     initial_sub = 0
+    topo_index = [[[] for i in range(switches)] for j in range(bipart_num)]
     for bipart in range(bipart_num,0,-1):
         degree = vir_layer_degree[bipart] - initial_sub
         degrees = [degree for i in range(switches)]
@@ -22,6 +23,7 @@ def fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random):
             random.shuffle(switch_list)
         for sw in range(switches):
             src = switch_list[sw]
+            topo_index[bipart-1][src].append(src)
             for link in range(degree):
                 if(basic == sw):
                     remain_list.append(sw)
@@ -30,7 +32,6 @@ def fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random):
                 while(degrees[dst] == 0):
                     basic = change_base(basic, switches)
                     dst = switch_list[basic]
-
                 if(len(remain_list) > 0):
                     if(remain_list[0] != sw):
                         dst = switch_list[remain_list[0]]
@@ -41,18 +42,71 @@ def fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random):
                 else:
                     basic = change_base(basic, switches)
                 degrees[dst] = degrees[dst] - 1
+                topo_index[bipart-1][src].append(dst)
                 src_port = remain_ports[src].pop(0)
                 dst_port = remain_ports[dst].pop(0)
-                print(src, dst, src_port, dst_port)
         initial_sub = degree
-        print(bipart)
+    return topo_index
 
+def search_path(topo_index, root, topo_dic):
+    root_path = [[root]]
+    bipart_num = len(topo_index)
+    drop_num = 0
+    for bipart in range(bipart_num-1,-1,-1):
+        root_num = len(root_path)
+        for path_num in range(root_num):
+            path = root_path.pop(0)
+            layer_root = path[-1]
+            tp_index = topo_index[bipart][layer_root]
+            for link in tp_index:
+                if((bipart != 0) | (link != path[-1])):
+                    if((link not in path) | (link == path[-1])):
+                        path.append(link)
+                        root_path.append([i for i in path])
+                        path.pop()
+                    else:
+                        if(bipart == 0):
+                            drop_num += 1
+                else:
+                    if(bipart == 0):
+                        drop_num += 1
+            if(drop_num == len(tp_index)):
+                root_path.append([i for i in path])
+            drop_num = 0
+    # 构造两层字典比单纯列表消耗更大
+    for i in range(len(root_path)):
+        path = root_path[i]
+        for node_num in range(len(path)):
+            if(path[node_num] != root):
+                if(root not in topo_dic[path[node_num]].keys()):
+                    topo_dic[path[node_num]][root] = [(i,node_num)]
+                else:
+                    topo_dic[path[node_num]][root].append((i,node_num))
+    return root_path,topo_dic
+
+def route_generate(topo_index, switches):
+    all_path = []
+    topo_dic = {}
+    for i in range(switches):
+        topo_dic[i] = {}
+    for sw in range(switches):
+        root_path,topo_dic= search_path(topo_index, sw, topo_dic)
+        all_path.append(root_path)
+        print(root_path)
+    print(topo_dic)
 
 
 if __name__ == "__main__":
-    switches = 10
+    switches = 3
     hosts = 24
     ports = 30
-    vir_layer_degree = [1, 3, 2]
+    vir_layer_degree = [1,2,2,1]
     is_random = 0
-    fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random)
+    # switches = 10000
+    # hosts = 24
+    # ports = 64
+    # vir_layer_degree = [5,10,10,10,5]
+    # is_random = 1
+    topo_index = fc_topo_gene(switches, hosts, ports, vir_layer_degree, is_random)
+    route_generate(topo_index, switches)
+
