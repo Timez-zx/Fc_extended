e542e20f74c9ddd4ec146342f1cc90d0ddfea107
张潇 <zhangxiao@zhangxiaodeMacBook-Pro-2.local>
2022-12-29 01:42:16 +0800

    by zx

--- topology/fc_topo_gene/fc_topo_all_route.py
+++ topology/fc_topo_gene/fc_topo_all_route.py
@@ -275,7 +275,7 @@ class Fc_topo_all_route():
                         print("Pro %s has saved %.4f data"%(multiprocessing.current_process().name, save_count/len(pairs)))
                     path_route.clear()
                     file_obj.flush()
-                    os.fsync(file_obj)
+                    os.fsync(file_obj.fileno())
         file_obj.close()
         print("Multi-pro %s has fininshed"%(multiprocessing.current_process().name))
 
