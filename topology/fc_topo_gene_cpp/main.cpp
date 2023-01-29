2d5f4e0d19050e7fe865a66f3051ef50aa77e821
zx <zhangxiao123@sjtu.edu.cn>
2023-01-29 23:59:43 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -18,31 +18,31 @@ int main(){
     // int is_random = 1;
     // int random_seed = 7;
 
-    int switches = 5;
+    int switches = 10;
     int hosts = 24;
-    int ports = 28;
-    int vir_layer_degree[] = {1, 2, 1};
-    int layer_num = 3;
+    int ports = 36;
+    int vir_layer_degree[] = {2, 4, 4, 2};
+    int layer_num = 4;
     int is_random = 1;
     int random_seed = 2;
 
     Fc_edge_disjoin_route fc_test(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed);
-    fc_test.fc_topo_gene_1v1();
-    // fc_test.path_infor_gene();
+    fc_test.fc_topo_gene();
+    fc_test.path_infor_gene();
     // fc_test.display_all_path();
-    // fc_test.build_search_dic();
+    fc_test.build_search_dic();
     // fc_test.display_dic(2);
 
 
     struct timeval start, end;
     gettimeofday(&start, NULL);
-    // bool if_report = true;
-    // int report_inter = 45;
-    // bool if_store = true;
-    // bool store_part = true;
-    // fc_test.pthread_for_all_route(1, if_report, report_inter, if_store, store_part);
+    bool if_report = true;
+    int report_inter = 45;
+    bool if_store = true;
+    bool store_part = true;
+    fc_test.pthread_for_all_route(1, if_report, report_inter, if_store, store_part);
 
-    // fc_test.find_edge_disjoin_route_fast(1, 0, 1, "sw10_vir2442_rand1", true);
+    fc_test.find_edge_disjoin_route_fast(1, 0, 1, "sw10_vir2442_rand2", true);
     // fc_test.find_edge_disjoin_route_fast(8, 2, 10000, "sw500_vir713137_rand7", true);
     // fc_test.find_edge_disjoin_route_fast(8, 2, 10000, "sw2000_vir51010105_rand2", true);
     gettimeofday(&end, NULL);
