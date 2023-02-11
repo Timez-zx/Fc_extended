e44ebdbed4e2aa71ab96add3ba3f409f2cfc0bf4
zx <zhangxiao123@sjtu.edu.cn>
2023-02-12 00:12:41 +0800

    by zx

--- topology/clos/clos_basic.py
+++ topology/clos/clos_basic.py
@@ -1,15 +1,16 @@
 import math
 
 class clos_basic():
-    def __init__(self, switches, hosts, ports):
+    def __init__(self, switches, servers, ports):
         self.switches = switches
-        self.hosts = hosts
+        self.servers = servers
         self.ports = ports
 
-    def cost_equivalent_clos(self, num_servers):
+    def cost_equivalent_clos(self):
         num_eps = self.switches
         eps_port_count = self.ports
-        # print(num_eps, num_servers, eps_port_count)
+        num_servers = self.servers
+        print(num_eps, num_servers, eps_port_count)
         # check 2-layered clos
         host_per_tor = math.ceil(num_servers / eps_port_count)
         l2_throughput = 0
@@ -27,13 +28,30 @@ class clos_basic():
             num_cores_per_group = math.ceil(eps_port_count / 2 / num_aggr_links_per_core)
             num_cores = num_cores_per_group * (eps_port_count - host_per_tor)
             if num_tors + num_aggrs + num_cores <= num_eps:
-                print(num_tors + num_aggrs + num_cores)
+                print(num_tors, num_aggrs, num_cores)
                 l3_throughput = (eps_port_count - host_per_tor) / host_per_tor
                 break
         return max(l2_throughput, l3_throughput)
+    
+    def bisection_topo(self, Tor_hosts):
+        eps_port_count = self.ports
+        num_servers = self.servers
+        mini_tor_host = eps_port_count // 2
+        for host_per_tor in range(eps_port_count // 2, eps_port_count):
+            num_tors = math.ceil(num_servers / host_per_tor)
+            num_pods = math.ceil(num_tors / eps_port_count * 2)
+            if num_pods > eps_port_count:
+                continue
+            mini_tor_host = host_per_tor
+            break
+        if(Tor_hosts < mini_tor_host):
+            print("Tor hosts must be larger than", mini_tor_host)
+            exit(1)
+
 
 if __name__ == "__main__":
 
-    clos_temp = clos_basic(148, 8, 32)
-    throughput = clos_temp.cost_equivalent_clos(1152)
-    print(throughput)
+    clos_temp = clos_basic(20000, 12000, 32)
+    clos_temp.bisection_topo(24)
+    # throughput = clos_temp.cost_equivalent_clos()
+    # print(throughput)
