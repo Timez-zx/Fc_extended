f7f4821859a04a03b1697bbddaa915d35b812842
zx <zhangxiao123@sjtu.edu.cn>
2023-02-13 22:34:01 +0800

    by zx

---
+++ topology/fc_topo_gene_cpp/src/fc_tagger_ksp.h
@@ -0,0 +1,18 @@
+#ifndef FC_TAGGER_KSP_
+#define FC_TAGGER_KSP_
+
+
+#include "fc_base.h"
+
+class Fc_tagger_ksp: public Fc_base{
+    public:
+        Fc_tagger_ksp(int switches, int hosts, int ports, int* vir_layer_degree, int layer_num, int is_random, int random_seed):
+        Fc_base(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed){}
+
+        ~Fc_tagger_ksp(){};
+
+        
+
+}
+
+#endif
