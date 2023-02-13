20ea413d4ae5fa420c0d99f71bc5d9498f174f46
zx <zhangxiao123@sjtu.edu.cn>
2023-02-13 23:26:48 +0800

    by zx

--- topology/fc_topo_gene_cpp/src/fc_tagger_ksp.h
+++ topology/fc_topo_gene_cpp/src/fc_tagger_ksp.h
@@ -8,10 +8,12 @@ class Fc_tagger_ksp: public Fc_base{
     public:
         Fc_tagger_ksp(int switches, int hosts, int ports, int* vir_layer_degree, int layer_num, int is_random, int random_seed):
         Fc_base(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed){}
-
         ~Fc_tagger_ksp(){};
-
         void save_graph_infor();
+    
+    private:
+        unordered_map<int,int> link_to_layer_map;
+
 
 
 
