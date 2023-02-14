2c0dc5a6374d3097efa28a1aa1ef8b967473a8c9
zx <zhangxiao123@sjtu.edu.cn>
2023-02-15 06:43:00 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -39,24 +39,24 @@ int main(){
     // int is_random = 1;
     // int random_seed = 2;
 
-    int switches = 4500;
-    int hosts = 16;
-    int ports = 32;
-    int vir_layer_degree[] = {2, 6, 6, 2};
-    int layer_num = 4;
-    int is_random = 1;
-    int random_seed = 3;
-
-    // int switches = 250;
-    // int hosts = 14;
+    // int switches = 4500;
+    // int hosts = 16;
     // int ports = 32;
-    // int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
-    // int layer_num = 6;
+    // int vir_layer_degree[] = {2, 6, 6, 2};
+    // int layer_num = 4;
     // int is_random = 1;
-    // int random_seed = 5;
+    // int random_seed = 3;
+
+    int switches = 400;
+    int hosts = 14;
+    int ports = 32;
+    int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
+    int layer_num = 6;
+    int is_random = 1;
+    int random_seed = 5;
     // Fc_edge_disjoin_cost_route fc_test(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed);
     Fc_tagger_ksp fc_test(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed);
-    fc_test.fc_topo_gene_1v1(1);
+    fc_test.fc_topo_gene_1v1(0);
     // fc_test.save_graph_infor();
 
     // fc_test.path_infor_gene();
@@ -67,7 +67,7 @@ int main(){
     int report_inter = 100;
     bool if_store = true;
     bool store_part = false;
-    // fc_test.pthread_up_down_ksp(16, 32, 100, if_report, report_inter, if_store);
+    fc_test.pthread_up_down_ksp(1, 32, 100, if_report, report_inter, if_store);
     // fc_test.pthread_up_down_ksp(16, 32, 3, if_report, report_inter, if_store);
     // fc_test.pthread_up_down_ksp(16, 32, 2, if_report, report_inter, if_store);
     // fc_test.pthread_for_all_route(4, if_report, report_inter, if_store, store_part);
@@ -94,7 +94,7 @@ int main(){
     // tranceiver_cost[2] = 1000; 
     // fc_test.cost_model(ocs_ports, distance_infor, copper_cost, fiber_cost, tranceiver_cost);
 
-    fc_test.multi_thread_b_bandwidth(16, 50, 20, 1000);
+    // fc_test.multi_thread_b_bandwidth(16, 50, 20, 1000);
     gettimeofday(&end, NULL);
     cout << "Time use: " << (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/double(1e6) << "s" << endl;
     return 0;
