1789182fd75acb31ed534e6410492b2fc33f6421
zx <zhangxiao123@sjtu.edu.cn>
2023-02-14 22:39:30 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -37,7 +37,7 @@ int main(){
     // int layer_num = 4;
     // int is_random = 1;
     // int random_seed = 2;
-    int switches = 400;
+    int switches = 300;
     int hosts = 14;
     int ports = 32;
     int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
@@ -57,7 +57,7 @@ int main(){
     int report_inter = 100;
     bool if_store = true;
     bool store_part = false;
-    fc_test.pthread_up_down_ksp(1, 32, 100, if_report, report_inter, if_store);
+    // fc_test.pthread_up_down_ksp(1, 32, 100, if_report, report_inter, if_store);
     // fc_test.pthread_for_all_route(4, if_report, report_inter, if_store, store_part);
     // string mode = "ksp";
     // int ksp_num = 32;
@@ -66,7 +66,7 @@ int main(){
     // bool if_search_map = true;
     // fc_test.find_all_route(4, 5000, if_search_map);
     // double wr_ave = fc_test.throughput_test("wr", 2);
-    // double wr_ave = fc_test.throughput_test_ksp("wr", 2, 32, 2);
+    double wr_ave = fc_test.throughput_test_ksp("wr", 2, 32, 3);
 
     // int ocs_ports = 400;
     // int distance_infor[4];
