4c3b01d09368da247f83a8a55ebbd9c2f7aa5a18
zx <zhangxiao123@sjtu.edu.cn>
2023-02-15 21:17:57 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -47,7 +47,7 @@ int main(){
     // int is_random = 1;
     // int random_seed = 3;
 
-    int switches = 250;
+    int switches = 400;
     int hosts = 14;
     int ports = 32;
     int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
@@ -77,7 +77,7 @@ int main(){
     // bool if_search_map = true;
     // fc_test.find_all_route(4, 5000, if_search_map);
     // double wr_ave = fc_test.throughput_test("wr", 2);
-    double wr_ave = fc_test.throughput_test("wr", 2, 32, 2);
+    double wr_ave = fc_test.throughput_test("wr", 2, 32, 100);
 
     // int ocs_ports = 400;
     // int distance_infor[4];
