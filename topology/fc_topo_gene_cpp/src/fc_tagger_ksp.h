aca79f30c8c2296ce4bc018ec63cc3cd224962e6
zx <zhangxiao123@sjtu.edu.cn>
2023-02-16 19:10:56 +0800

    by zx

--- topology/fc_topo_gene_cpp/src/fc_tagger_ksp.h
+++ topology/fc_topo_gene_cpp/src/fc_tagger_ksp.h
@@ -8,19 +8,29 @@
 #include "KSP/DijkstraShortestPathAlg.h"
 #include "KSP/YenTopKShortestPathsAlg.h"
 
+/*
+Fc_tagger_ksp: select the k shortest paths whose down-up times are smaller than (the lossless queues - 1)
+we can use, the number of lossless queue is named by vc_num. 
+*/
+
 class Fc_tagger_ksp: public Fc_base{
     public:
         Fc_tagger_ksp(int switches, int hosts, int ports, int* vir_layer_degree, int layer_num, int is_random, int random_seed):
         Fc_base(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed){}
         ~Fc_tagger_ksp(){};
+        
+        // Save the topo infor in files for ksp function interface 
         void save_graph_infor();
 
         string gene_path_for_file_ksp(string path, int ksp_num, int vc_num);
 
+        // Use multi threads to calculate ksp-paths with small down-up times
+        // path_num: the number of shortest paths vc_num: the number of lossless queues we can use
+        void pthread_up_down_ksp(int thread_num, int path_num, int vc_num, bool if_report, int report_inter, bool if_store);
         uint16_t search_up_down_ksp(int src, int dst, int path_num, int vc_num, uint16_t *path_infor);
         void thread_up_down_ksp(vector<int*> route_pairs, int thread_label, int path_num, int vc_num, bool if_report, int report_inter, bool if_store, string store_file);
-        void pthread_up_down_ksp(int thread_num, int path_num, int vc_num, bool if_report, int report_inter, bool if_store);
-    
+
+        // redefine the throughput because of different parameters
         double throughput_test_ksp(string type, int seed, int path_num, int vc_num);
 
     private:
