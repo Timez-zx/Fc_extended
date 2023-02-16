632db34fb34eccc6895251460adcf13d4651b355
zx <zhangxiao123@sjtu.edu.cn>
2023-02-16 17:48:35 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -22,13 +22,13 @@ int main(){
     // int is_random = 1;
     // int random_seed = 1;
 
-    int switches = 144;
-    int hosts = 8;
-    int ports = 32;
-    int vir_layer_degree[] = {4, 8, 8, 4};
-    int layer_num = 4;
-    int is_random = 1;
-    int random_seed = 1;
+    // int switches = 144;
+    // int hosts = 8;
+    // int ports = 32;
+    // int vir_layer_degree[] = {4, 8, 8, 4};
+    // int layer_num = 4;
+    // int is_random = 1;
+    // int random_seed = 1;
 
 
     // int switches = 6000;
@@ -47,17 +47,17 @@ int main(){
     // int is_random = 1;
     // int random_seed = 1;
 
-    // int switches = 100;
-    // int hosts = 14;
-    // int ports = 32;
-    // int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
-    // int layer_num = 6;
-    // int is_random = 1;
-    // int random_seed = 5;
+    int switches = 500;
+    int hosts = 14;
+    int ports = 32;
+    int vir_layer_degree[] = {2, 3, 4, 4, 3, 2};
+    int layer_num = 6;
+    int is_random = 1;
+    int random_seed = 5;
     // Fc_edge_disjoin_cost_route fc_test(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed);
     Fc_tagger_ksp fc_test(switches, hosts, ports, vir_layer_degree, layer_num, is_random, random_seed);
     fc_test.fc_topo_gene_1v1(0);
-    // fc_test.save_graph_infor();
+    fc_test.save_graph_infor();
     // fc_test.path_infor_gene();
     // fc_test.build_search_dic();
 
@@ -66,9 +66,9 @@ int main(){
     int report_inter = 100;
     bool if_store = true;
     bool store_part = false;
-    // fc_test.pthread_up_down_ksp(16, 32, 2, if_report, report_inter, if_store);
-    // fc_test.pthread_up_down_ksp(16, 32, 3, if_report, report_inter, if_store);
-    // fc_test.pthread_up_down_ksp(16, 32, 100, if_report, report_inter, if_store);
+    fc_test.pthread_up_down_ksp(16, 32, 2, if_report, report_inter, if_store);
+    fc_test.pthread_up_down_ksp(16, 32, 3, if_report, report_inter, if_store);
+    fc_test.pthread_up_down_ksp(16, 32, 100, if_report, report_inter, if_store);
     // fc_test.pthread_for_all_route(4, if_report, report_inter, if_store, store_part);
     // string mode = "ksp";
     // int ksp_num = 32;
@@ -79,14 +79,14 @@ int main(){
     // double wr_ave = fc_test.throughput_test("wr", 2);
     // double wr_ave = fc_test.throughput_test("wr", 2, 32, 2);
 
-    int ocs_ports = 400;
-    int distance_infor[4];
-    distance_infor[0] = 2; // Tor to tor:x
-    distance_infor[1] = 2; // Tor to tor:y
-    distance_infor[2] = 2; // Ocs to tor:x
-    distance_infor[3] = 2;  // Ocs to ocs:y
-    int column_num = 50;
-    fc_test.cost_model(ocs_ports, distance_infor, column_num);
+    // int ocs_ports = 400;
+    // int distance_infor[4];
+    // distance_infor[0] = 2; // Tor to tor:x
+    // distance_infor[1] = 2; // Tor to tor:y
+    // distance_infor[2] = 2; // Ocs to tor:x
+    // distance_infor[3] = 2;  // Ocs to ocs:y
+    // int column_num = 50;
+    // fc_test.cost_model(ocs_ports, distance_infor, column_num);
 
     // fc_test.multi_thread_b_bandwidth(16, 50, 20, 1000);
     gettimeofday(&end, NULL);
