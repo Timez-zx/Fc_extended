064de7b14a816447a887d9b80fc874ccaca536d2
zx <zhangxiao123@sjtu.edu.cn>
2023-02-19 23:44:24 +0800

    by zx

--- topology/fc_topo_gene_cpp/src/TAGGER/graph_tag.h
+++ topology/fc_topo_gene_cpp/src/TAGGER/graph_tag.h
@@ -4,26 +4,12 @@
 #include <iostream>
 #include <vector>
 
-using SwitchId = int;
-using PortId = int;
-using TagId = int;
+using TaggerNodeId = int;
 
-typedef struct _SPNode{
-    _SPNode(SwitchId switch_in, PortId port_in, TagId tag_in, int index_in):
-           switchId(switch_in), portId(port_in), tagId(tag_in), FirstIndex(index_in){}
-    ~_SPNode(){}
-    SwitchId switchId;
-    PortId portId;
-    TagId tagId;
-    int FirstIndex;
-} SPNode;
 
 typedef struct _SPLink{
-    _SPLink(SPNode dst_in, int index_in): 
-            dstNode(dst_in), NextIndex(index_in){}
-    ~_SPLink(){}
-    SPNode dstNode;
-    int NextIndex;
+    TaggerNodeId toNode;
+    int nextEdgeIdex;
 } SPLink;
 
 class TaggerGraph{
