058b067033ef4a7144c135926f185370b7d30f29
zx <zhangxiao123@sjtu.edu.cn>
2023-03-04 22:23:45 +0800

    by zx

--- topology/fc_topo_gene_cpp/main.cpp
+++ topology/fc_topo_gene_cpp/main.cpp
@@ -1,7 +1,6 @@
 #include <sys/time.h>
 #include <iostream>
-#include "src/fc_edge_disjoin_route.h"
-#include "src/fc_edge_disjoin_cost_route.h"
+#include "src/fc_topo_all_route.h"
 #include "src/fc_tagger_kspz.h"
 #include "src/fc_tagger_test.h"
  
@@ -10,44 +9,44 @@ using namespace std;
 
 
 // There are some demos in the demo, please refer file "demo".
-double up_down_ksp_throught(Fc_topo_all_route& fc){
-    fc.fc_topo_gene_1v1(0);
-    fc.path_infor_gene();
-    fc.build_search_dic();
-    bool if_report = true;
-    int report_inter = 5000;
-    bool if_store = true;
-    string mode = "ksp";
-    int ksp_num = 32;
-    fc.pthread_for_all_path(16, if_report, report_inter, if_store, mode, ksp_num);
-    return fc.throughput_test("wr", 2);
-} 
+// double up_down_ksp_throught(Fc_topo_all_route& fc){
+//     fc.fc_topo_gene_1v1(0);
+//     fc.path_infor_gene();
+//     fc.build_search_dic();
+//     bool if_report = true;
+//     int report_inter = 5000;
+//     bool if_store = true;
+//     string mode = "ksp";
+//     int ksp_num = 32;
+//     fc.pthread_for_all_path(16, if_report, report_inter, if_store, mode, ksp_num);
+//     return fc.throughput_test("wr", 2);
+// } 
 
 
-double edge_disjoint_throught(Fc_edge_disjoin_route& fc){
-    fc.fc_topo_gene_1v1(0);
-    fc.path_infor_gene();
-    fc.build_search_dic();
-    bool if_report = true;
-    int report_inter = 5000;
-    bool if_store = true;
-    bool store_part = false;
-    fc.pthread_for_all_route(16, if_report, report_inter, if_store, store_part);
-    fc.find_all_route(16, 5000);
-    return fc.throughput_test("wr", 2);
-} 
+// double edge_disjoint_throught(Fc_edge_disjoin_route& fc){
+//     fc.fc_topo_gene_1v1(0);
+//     fc.path_infor_gene();
+//     fc.build_search_dic();
+//     bool if_report = true;
+//     int report_inter = 5000;
+//     bool if_store = true;
+//     bool store_part = false;
+//     fc.pthread_for_all_route(16, if_report, report_inter, if_store, store_part);
+//     fc.find_all_route(16, 5000);
+//     return fc.throughput_test("wr", 2);
+// } 
 
 
-double ksp_tagger_throught(Fc_tagger_kspz& fc, int path_num, int vc_num){
-    fc.fc_topo_gene_1v1(0);
-    fc.save_graph_infor();
-    bool if_report = true;
-    int report_inter = 100000;
-    bool if_store = true;
-    bool store_part = false;
-    fc.pthread_up_down_ksp(16, path_num, vc_num, if_report, report_inter, if_store);
-    return fc.throughput_test_ksp("wr", 2, path_num, vc_num);
-}
+// double ksp_tagger_throught(Fc_tagger_kspz& fc, int path_num, int vc_num){
+//     fc.fc_topo_gene_1v1(0);
+//     fc.save_graph_infor();
+//     bool if_report = true;
+//     int report_inter = 100000;
+//     bool if_store = true;
+//     bool store_part = false;
+//     fc.pthread_up_down_ksp(16, path_num, vc_num, if_report, report_inter, if_store);
+//     return fc.throughput_test_ksp("wr", 2, path_num, vc_num);
+// }
 
 
 int main(int argc, char* argv[]){
